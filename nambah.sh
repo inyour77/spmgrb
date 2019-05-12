06c4d17e537ca156f523cf18c0dcf8bacb4bb63a
inyour77 <50392504+inyour77@users.noreply.github.com>
2019-05-12 12:42:26 +0700

    Add files via upload

---
+++ nambah.sh
@@ -0,0 +1,82 @@
+#whaat
+#jan
+#di
+#recode
+#sunceuir33"#
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+ye='\033[33;1m'
+clear
+sleep 3
+pkg update && upgrade
+figlet -f big 1|lolcat
+echo "TUNGGU 3 DETIK ASW >•<"|lolcat
+sleep 7
+clear
+pkg install python2 figlet
+figlet -f big 2|lolcat
+echo "TUNNGU 2 DETIK LAGI "|lolcat
+sleep 7
+clear
+gem install lolcat
+figlet -f big 3|lolcat
+echo "SATU DETIK LAGI >¤<"|lolcat
+sleep 4
+clear
+toilet SELESAY|lolcat
+clear
+echo "[\•/]===========~~~~~~~==============[\•/]"|lolcat
+echo "  |  Nick = InYourXerXez               |"|lolcat
+echo "  | Team  = Gblk Crew && 2e4h && LIT   |"|lolcat
+echo "  | Name  = TOOLS TAMBAHAN SPAM GRAB   |"|lolcat
+echo "[\•/]===========~~~~~~~==============[\•/]"|lolcat
+echo ""
+echo " |==========$=$===============|"|lolcat
+echo " |    (Y) Lanjut/(N) Stop     |"|lolcat
+echo " |==========$=$===============|"|lolcat
+echo $ye ""
+echo    "   ╭─[INYOUR~XERXEZ7]"
+read -p "   ╰─root@2asy4goblok~# " gblk;
+
+
+if [ $gblk = Y ] || [ $gblk = y ]
+then
+clear
+pkg update
+pkg upgrade
+pkg install git
+pkg install figlet
+gem install lolcat
+pkg install python2
+pkg install python
+pkg install php
+git clone https://github.com/inyour77/GBLKcerew
+cd GBLKcerew
+sh gb.sh
+fi
+
+if [ $gblk = N ] || [ $gblk = n ]
+then
+clear
+figlet -f big exiit|lolcat
+sleep 4
+exit
+fi
